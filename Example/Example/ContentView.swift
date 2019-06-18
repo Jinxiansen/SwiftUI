f8761d5951c96ec6b20747772b80f9503799e743
晋先森 <hi@jinxiansen.com>
2019-06-19 00:04:01 +0800

    # Update ContentView.

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -140,7 +140,7 @@ struct ContentView : View {
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().colorScheme(.dark)
     }
 }
 #endif
